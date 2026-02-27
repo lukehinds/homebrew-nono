2f88f6bf00743d7fc72b09da107ef53a8e9597bb
Luke Hinds <lukehinds@gmail.com>
2026-02-27 21:57:45 +0000

    Correct repo org
    Signed-off-by: Luke Hinds <lukehinds@gmail.com>

--- Formula/nono.rb
+++ Formula/nono.rb
@@ -1,16 +1,16 @@
 class Nono < Formula
   desc "Capability-based shell for AI agents with OS-enforced sandboxing"
-  homepage "https://github.com/lukehinds/nono"
+  homepage "https://github.com/always-further/nono"
   version "0.5.0"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/lukehinds/nono/releases/download/v0.5.0/nono-v0.5.0-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/always-further/nono/releases/download/v0.5.0/nono-v0.5.0-aarch64-apple-darwin.tar.gz"
       sha256 "5269ab5357c21d36cee70c3c6a499cd2538d34de03229f2c9c0f4915568e9d88"
     end
     on_intel do
-      url "https://github.com/lukehinds/nono/releases/download/v0.5.0/nono-v0.5.0-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/always-further/nono/releases/download/v0.5.0/nono-v0.5.0-x86_64-apple-darwin.tar.gz"
       sha256 "20345a92a8c6ab7c998c102f4a621a8945c27e1b91908dcc186cb7a624f8b8cd"
     end
   end
